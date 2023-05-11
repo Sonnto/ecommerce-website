01a51c6d769fad9455a1fb3c0992561047383f41
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-11 12:10:44 -0400

    add banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
