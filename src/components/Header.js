954279564dfc09a6096a2ca7277e9ef0a7e44b97
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-10 22:44:41 -0400

    import menu icon, search icon, shopping cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,35 @@
 import Image from "next/image";
+import {
+  MenuIcon,
+  MagnifyingGlassIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
 
 function Header() {
   return (
     <header>
       {/* TOP NAV */}
-      <div>
-        <div>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
+            objectFit="contain" //keeps aspect ratio
+            className="cursor-pointer" //tailwind here
           />
         </div>
+
+        {/* SEARCH BAR */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <MagnifyingGlassIcon className="h-12 p-4" />
+        </div>
       </div>
+
       {/* BOTTOM NAV */}
       <div></div>
     </header>
