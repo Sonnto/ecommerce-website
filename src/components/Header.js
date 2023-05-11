49fc29e082ced1da39aa780c3716d4f4a4457cb4
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-11 10:54:05 -0400

    Complete design for right side of search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import {
-  MenuIcon,
+  Bars3Icon,
   MagnifyingGlassIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/24/outline";
@@ -27,6 +27,22 @@ function Header() {
             type="text"
           />
           <MagnifyingGlassIcon className="h-12 p-4" />
+        </div>
+
+        {/* ACCOUNTS, SHOPPING CART*/}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="cursor-pointer">
+            <p>Hello, Sonnto!</p>
+            <p>Account & Lists</p>
+          </div>
+          <div className="cursor-pointer">
+            <p>Returns</p>
+            <p>& Orders</p>
+          </div>
+          <div className="cursor-pointer">
+            <ShoppingCartIcon className="h-10" />
+            <p>Basket</p>
+          </div>
         </div>
       </div>
 
