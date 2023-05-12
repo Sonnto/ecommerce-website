3f4bb1fad7bda1091c8664002bf208208a618c7f
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-12 13:33:29 -0400

    add product component to map products in json

--- src/components/ProductFeed.jsx
+++ src/components/ProductFeed.jsx
@@ -1,10 +1,20 @@
+import Product from "./Product";
+
 function ProductFeed({ products }) {
   return (
     <div>
       <h1>Products Here...</h1>
-      {console.log("Here:" + products)}
-      {products.map((product) => (
-        <p>{product.title}</p>
+      {console.log(`Checking if products be working: ${products}`)}
+      {products.map(({ id, title, price, description, category, image }) => (
+        <Product
+          key={id} //key (id) for React to identify products and map efficiently
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
       ))}
     </div>
   );
