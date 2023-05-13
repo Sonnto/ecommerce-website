419e6ee54478a63ef1ef2570a4b3aa8b84348da2
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 16:50:33 -0400

    fix hydrating errors
    - random rating and prime potentially caused server & client rendered UIs to be different

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -16,12 +16,18 @@ function Product({
   rate,
   count,
 }) {
-  const [randomRating] = useState(
-    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) +
-      MIN_RATING /* ensure min value of range starts at min rating value */
-  ); /*Generates number of max range and min range of 5 & 1;*/
+  const rating = [];
 
-  const [hasPrime] = useState(Math.random() < 0.5);
+  for (let i = 0; i < rate; i++) {
+    rating.push(<StarIcon className="text-yellow-500 h-5" key={i} />);
+  }
+  // const [randomRating] = useState(
+  //   Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) +
+  //     MIN_RATING /* ensure min value of range starts at min rating value */
+  // ); /*Generates number of max range and min range of 5 & 1;*/
+
+  const [hasPrime] = "Yes";
+  // const [hasPrime] = useState(Math.random() < 0.5);
   /* Randomizing whether item has prime; if the number between 0-1 is less than 0.5, it has prime*/
 
   return (
@@ -30,21 +36,13 @@ function Product({
         {category}
       </p>
       {/* Next.js image tag */}
-      <Image src={image} height={200} width={200} objectFit="contain" />
+      <Image src={image} height={200} width={200} style="contain" />
 
       <h4 className="my-3">{title}</h4>
-      {/* <p>
-        {rate} {count}
-      </p> */}
+
       <div className="flex">
-        {/* Array of [rating], filled with empty values, and map through those values; gives 2 values, second is index; amount of stars is decided by rating variable that is randomized*/}
-        {/*  */}
-        {Array(randomRating)
-          .fill()
-          .map((_, i) => (
-            <StarIcon className="text-yellow-500 h-5" />
-          ))}
-        <p className="ml-5 text-sm">{count} ratings</p>
+        {rating}
+        <p className="ml-4 text-sm leading-6">{count} ratings</p>
       </div>
 
       <p className="text-xs mb-2 mt-2 line-clamp-2">{description}</p>
