d1d323e7a7ee971e401cea263931d34070d5fb5a
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 17:34:59 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpYrd5HGjBlVFgpIZTyqvB4BhRDlQf1WY",
+  authDomain: "ecommerce--clone-dff35.firebaseapp.com",
+  projectId: "ecommerce--clone-dff35",
+  storageBucket: "ecommerce--clone-dff35.appspot.com",
+  messagingSenderId: "234576311632",
+  appId: "1:234576311632:web:a323a426b1b21f2d08e855",
+  measurementId: "G-YF522GW4HN",
+};
