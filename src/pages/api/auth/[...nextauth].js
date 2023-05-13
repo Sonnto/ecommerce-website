ddf9c3e717b0e184e9cdc2f2b3ce92c1f2f4efd4
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 17:38:03 -0400

    Update [...nextauth].js
    - remove github login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,10 +8,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     // ...add more providers here
   ],
   // A database is optional but required to persist accounts in a datbaase
