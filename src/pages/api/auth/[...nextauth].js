bbbc46dd3dbf353ff6acdd14d0bab42670b7adb8
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 17:34:06 -0400

    Create [...nextauth].js

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  // A database is optional but required to persist accounts in a datbaase
+  database: process.env.DATABASE_URL,
+});
