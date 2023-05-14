dbb97ace2685c84fca6ac50b4518e2d278f0d7f5
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 23:10:58 -0400

    add space for format

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { store } from "../app/store";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <SessionProvider session={pageProps.session}>
