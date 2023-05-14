4c32117b848f821337161f4397919da25d5d1543
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-14 16:00:09 -0400

    shows # cart items

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,7 +1,12 @@
 import Header from "../components/Header";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/cartSlice";
+import CheckoutProduct from "../components/CheckoutProduct";
 
 function Checkout() {
+  const items = useSelector(selectItems);
+
   return (
     <div className="bg-gray-100">
       <Header />
@@ -16,7 +21,15 @@ function Checkout() {
             objectFit="contain"
           />
           <div className="flex flex-col p-5 space-y-10 bg-white">
-            <h1 className="text-3xl border-b pb-4">Your Shopping Cart</h1>
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "Your Shopping Cart is Empty"
+                : "Shopping Cart"}
+            </h1>
+
+            {items.map((item, i) => (
+              <CheckoutProduct />
+            ))}
           </div>
         </div>
 
