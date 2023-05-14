b4a2061e5466d162423ffc397b9d87d58688641b
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-14 15:59:47 -0400

    Create CheckoutProduct.jsx

---
+++ src/components/CheckoutProduct.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function CheckoutProduct({
+  id,
+  title,
+  price,
+  rating,
+  description,
+  category,
+  image,
+  rate,
+  count,
+  hasPrime,
+}) {
+  return <div>CheckoutProduct</div>;
+}
+
+export default CheckoutProduct;
