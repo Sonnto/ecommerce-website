551469da1bac72b77bb3bf1a2a0e4d9f9da8f497
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-14 12:07:20 -0400

    add shopping cart stylization

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -8,13 +8,16 @@ function Checkout() {
 
       <main className="lg:flex max-w-screen-2xl mx-auto">
         {/* CHECKOUT LEFT SIDE */}
-        <div>
+        <div className="flex-grow m-5 shadow-sm">
           <Image
             src="/images/prime-day-banner.webp"
             width={1020}
             height={250}
             objectFit="contain"
           />
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">Your Shopping Cart</h1>
+          </div>
         </div>
 
         {/* CHECKOUT RIGHT SIDE */}
