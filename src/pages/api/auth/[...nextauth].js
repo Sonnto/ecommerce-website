1bf1eb23a0e9d740c7e4ad6e6a6837a9bb2be599
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 22:16:09 -0400

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,7 +2,24 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth({
+// export default NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+//   // A database is optional but required to persist accounts in a datbaase
+//   database: process.env.DATABASE_URL,
+// });
+
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -15,22 +32,5 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  // A database is optional but required to persist accounts in a datbaase
-  database: process.env.DATABASE_URL,
-});
-
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//     }),
-//     // ...add more providers here
-//   ],
-// };
-// export default NextAuth(authOptions);
+};
+export default NextAuth(authOptions);
