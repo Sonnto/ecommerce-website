02441b2a2346de59db17d13b5b907a5934fdf5fb
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-13 23:33:41 -0400

    Create checkout.js

---
+++ src/pages/checkout.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+function Checkout() {
+  return (
+    <div className="bg-gray-100">
+      <Header />
+
+      <h1>THE CHECKOUT PAGE</h1>
+    </div>
+  );
+}
+
+export default Checkout;
