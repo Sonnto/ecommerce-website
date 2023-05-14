92cf460c1417f2d0426d10d3a34ae63634df3e63
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-14 11:35:38 -0400

    build banner portion

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,25 @@
 import Header from "../components/Header";
+import Image from "next/image";
 
 function Checkout() {
   return (
     <div className="bg-gray-100">
       <Header />
 
-      <h1>THE CHECKOUT PAGE</h1>
+      <main className="lg:flex max-w-screen-2xl mx-auto">
+        {/* CHECKOUT LEFT SIDE */}
+        <div>
+          <Image
+            src="/images/prime-day-banner.webp"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
+        </div>
+
+        {/* CHECKOUT RIGHT SIDE */}
+        <div></div>
+      </main>
     </div>
   );
 }
