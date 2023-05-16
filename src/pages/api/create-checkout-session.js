d8861da6eac9ff3e3283963008a864d6d4fb10b5
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-16 14:24:02 -0400

    Debug: fix image array and shipping_options array
    - identified problem of array to Stripe API; issue with images and shipping options; fixed

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -14,11 +14,7 @@ export default async (req, res) => {
       product_data: {
         name: item.title,
         description: item.description,
-        images: item.image,
-        metadata: {
-          email,
-          images: JSON.stringify(items.map((item) => item.image)),
-        }, //to push values/data back to Firebase afterwards
+        images: [item.image],
       },
     },
     quantity: 1,
@@ -36,10 +32,11 @@ export default async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     shipping_options: [
-      "shr_1N8DnGBgOzp49Zd0QNUP8GtK",
-      "shr_1N8DS0BgOzp49Zd0kzj6Qxyv",
-      "shr_1N8Dr8BgOzp49Zd0QFDfAkNG",
-    ], //from stripe's website dashboard for project ecommerce-website
+      "shipping_rate"["shr_1N8DnGBgOzp49Zd0QNUP8GtK"],
+      "shipping_rate"["shr_1N8DS0BgOzp49Zd0kzj6Qxyv"],
+      "shipping_rate"["shr_1N8Dr8BgOzp49Zd0QFDfAkNG"],
+    ],
+    //from stripe's website dashboard for project ecommerce-website
     shipping_address_collection: {
       allowed_countries: ["CA", "US", "GB", "HK"],
     },
@@ -47,8 +44,12 @@ export default async (req, res) => {
     mode: "payment",
     success_url: `${process.env.HOST}/success`,
     cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    }, //to push values/data back to Firebase afterwards
   });
-  console.log("after session await");
+  console.log("after session async function");
   console.log(transformedItems);
   res.status(200).json({ id: session.id });
 };
