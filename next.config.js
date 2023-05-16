6d3004190ad3645467c33e7e0ad7b0aa813a9fb4
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-15 20:41:05 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   images: {
-    /* whitelisting these domains for dummy test data*/
-    domains: ["links.papareact.com", "fakestoreapi.com"],
+    /* whitelisted domains for test data*/
+    domains: ["fakestoreapi.com"],
+  },
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
 };
