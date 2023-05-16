49c905afd471fb079fd7389fbc4c0c3db2e5335d
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-16 00:29:13 -0400

    Create create-checkout-session.js
    - current bug is invalid array for stripe api; checked documentation and it's all correct... unsure what's up.

---
+++ src/pages/api/create-checkout-session.js
@@ -0,0 +1,54 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async (req, res) => {
+  const { items, email } = req.body;
+  //destructure and pull out those values; same as `req.body.items` and `req.body.email`
+
+  console.log(items);
+
+  const transformedItems = items.map((item) => ({
+    //implicit return
+    price_data: {
+      currency: "CAD",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        description: item.description,
+        images: item.image,
+        metadata: {
+          email,
+          images: JSON.stringify(items.map((item) => item.image)),
+        }, //to push values/data back to Firebase afterwards
+      },
+    },
+    quantity: 1,
+  }));
+
+  console.log(transformedItems);
+  console.log(
+    transformedItems.map((item) => item.price_data.product_data.images)
+  );
+  console.log(
+    transformedItems.map((item) => item.price_data.product_data.metadata)
+  );
+  console.log("right before the session await begins");
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_options: [
+      "shr_1N8DnGBgOzp49Zd0QNUP8GtK",
+      "shr_1N8DS0BgOzp49Zd0kzj6Qxyv",
+      "shr_1N8Dr8BgOzp49Zd0QFDfAkNG",
+    ], //from stripe's website dashboard for project ecommerce-website
+    shipping_address_collection: {
+      allowed_countries: ["CA", "US", "GB", "HK"],
+    },
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+  });
+  console.log("after session await");
+  console.log(transformedItems);
+  res.status(200).json({ id: session.id });
+};
