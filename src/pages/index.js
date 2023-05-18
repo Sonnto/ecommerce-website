c8c7968bd0beb26a943c7975086c7189d500da49
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-18 09:54:37 -0400

    Include session to pass through

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductSect from "../components/ProductSect";
+import { getSession } from "next-auth/react";
 
 export default function Home({ products }) {
   return (
@@ -26,12 +27,14 @@ export default function Home({ products }) {
 // FAKE STORE API to populate items being sold, using Next.js
 //GET request to https://fakestoreapi.com/products
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
   return {
     props: {
       products,
+      session,
     },
   };
 }
